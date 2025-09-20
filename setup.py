8e86a70729d251bbd1c7ef41c6ee1660cc0d007f
Pandhari <pandhari2527@gmail.com>
2025-09-20 16:26:14 +0530

    proj struct and env done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Pandharinath Makse",
+    author_email="pandharinath1437@gmail.com",
+    packages=find_packages()
+)
